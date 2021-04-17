679b73e1326fbe9726b161bcdf9f63d7be9b9b75
PAEST-APP <80470160+PAEST-APP@users.noreply.github.com>
2021-04-18 01:01:04 +0200

    Add files via upload

---
+++ directori/assignatures/assignatures.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignatures',
+  templateUrl: './assignatures.page.html',
+  styleUrls: ['./assignatures.page.scss'],
+})
+export class AssignaturesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
